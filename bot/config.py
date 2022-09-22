d840d4d7484f1991e56d1357947ac0f32b475b1d
mhprotechbd <86700279+mhprotechbd@users.noreply.github.com>
2022-09-23 00:31:27 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,10 +1,10 @@
 class config:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
-    DATABASE_URL = ""
-    SUDO_USERS = "" # Sepearted by space.
-    SUPPORT_CHAT_LINK = ""
+    BOT_TOKEN = "5798171509:AAG-k_8b20z2ypLAX3gvlTy2J2vrPNmZ1W4"
+    APP_ID = "11469047"
+    API_HASH = "a29c7f1ef39fb5c3a9369fc3b05ba2b6"
+    DATABASE_URL = "postgres://ihvpchxxcmtztu:6ec34f2809f59f0a2b3d0c387452331f507e3ae79cf4c7c5ec3e225a9aadbcd1@ec2-34-194-40-194.compute-1.amazonaws.com:5432/ddt6243hd1htnf"
+    SUDO_USERS = "1204334024" # Sepearted by space.
+    SUPPORT_CHAT_LINK = "https://t.me/mhdriveg"
     DOWNLOAD_DIRECTORY = "./downloads/"
 
 
